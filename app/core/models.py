64e0a804985fefceb25438780da312b759d4bc06
JORGE ARMANDO <jorgeabm1992@gmail.com>
2021-07-17 01:48:09 -0500

    add model clases: Currency, Track_Fee

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Currency(models.Model):
+    id_currency = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=4)
+    exchange = models.FloatField()
+    fee_percentage = models.FloatField()
+    quantity = models.FloatField()
+
+    def __str__(self):
+        return super().__str__()
+
+class Track_Fee(models.Model):
+    id_track_fee = models.AutoField(primary_key=True)
+    fee_amount = models.FloatField()
+    date_transaction = models.CharField(max_length=45)
+    base_currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, related_name='base')
+    quote_currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, related_name='quote')
+
+    def __str__(self):
+        return super().__str__()
